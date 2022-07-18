76e48b1bcffa8a90aa0f5ca6584e4b96440eb52f
unknown <samelasiziba73@gmail.com>
2022-07-18 11:35:46 +0200

    transactions

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var state ={
  total :0,        
 }
 transactions;[
-           
+{ type: 'income',detail: salary,value:R7000},
+{ type: 'expense',detail: transport,value:R500},
+{type : 'expense',detail:petrol,value:R300}
 ]
 var income = document.getElementById('income');
 var expense= document.getElementById('expense');
