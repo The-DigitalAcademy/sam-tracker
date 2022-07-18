f6938bfa629e9693c479c0cd3e20c1abd0c0290f
unknown <samelasiziba73@gmail.com>
2022-07-18 09:03:56 +0200

    var state

---
+++ script.js
@@ -0,0 +1,11 @@
+var state ={
+ income :0,
+ expense: 0, 
+ total :0,        
+}
+ let type = document.getElementById('type').value;
+ let name = document.getElementById('name').value;
+ let amount = document.getElementById('amount').value;
+ let total=document.getElementById('total').value;
+
+
