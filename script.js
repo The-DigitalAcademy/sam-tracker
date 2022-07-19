8ba83c5d825de716460263e9eb57f0a61c59faee
unknown <samelasiziba73@gmail.com>
2022-07-19 15:32:11 +0200

    submit

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-var state ={
+var state = {
  income :0,
- expense: 0, 
- total :0,        
+ expense :0, 
+ transactions: [
+ {type:'income',detail:'salary',amount: 150},
+ {type:'expense',detail:'petrol',amount:300},
+ ],     
 }
-transactions;[
-{ type: 'income',detail: salary,value:R7000},
-{ type: 'expense',detail: transport,value:R500},
-{type : 'expense',detail:petrol,value:R300}
-]
-var income = document.getElementById('income');
-var expense= document.getElementById('expense');
-var name = document.getElementById('name');
-var amount = document.getElementById('amount');
-var type = document.getElementById('type');
 
+var income=document.getElementById('income')
+var expense=document.getElementById('expense')
 
+// display everything
+income.innerHTML=state.income
+expense.innerHTML=state.expense
