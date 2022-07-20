86593d8fa964bf0980e67b62f805c553a5469c9c
unknown <samelasiziba73@gmail.com>
2022-07-20 10:03:47 +0200

    addname

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 var state = {
+ balance:0,          
  income :0,
  expense :0, 
  transactions: [
- {type:'income',detail:'salary',amount: 150},
- {type:'expense',detail:'petrol',amount:300},
- ],     
+           {type:'income',detail:'salary',amount: 150},
+           {type:'expense',detail:'petrol',name:300},
+           {type:'expense',detail:'salon',amount:300},
+ ]    
 }
 
 var income=document.getElementById('income')
 var expense=document.getElementById('expense')
+var amount=document.getElementById('amount')
+
+function addName() {
+           name = document.getElementById('name').value
+           console.log(name);
+}
 
 // display everything
-income.innerHTML=state.income
-expense.innerHTML=state.expense
+function display() {}
+display();
