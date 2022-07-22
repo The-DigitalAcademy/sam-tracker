617a6fb7d4293ab16a5036ba309bc2b0d6a981c9
unknown <samelasiziba73@gmail.com>
2022-07-22 11:25:10 +0200

    var state

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 var state= {
- balance:0,
+ balance:700,
  income:0,
  expense:0,          
-transactions:[        
-{type : 'income', detail:'cash money',amount: 4000},
-{type : 'income', detail: 'cash money',amount: 4000},
-{type : 'income', detail: 'cash money', amount: 4000},
-],
+  data:[],
 }
-var balance = document.getElementById('balance')
-var income = document.getElementById('income')
-var expense = document.getElementById('expense')
-var name = document.getElementById('name')
-var amount = document.getElementById('amount')
+var balance = document.getElementById('balance').value
+var income = document.getElementById('income').value
+var expense = document.getElementById('expense').vlaue
+var name = document.getElementById('name').value
+var amount = document.getElementById('amount').value
 
 var money;
 var transaction;
 
-function addButton()
-
-
- if(isValid(money) && isValid(transaction)) {  
-    tableData.push(   
-        
-    )          
+function input() {
+  amount = Number(document.getElementById('amount').value);
+  transactionName =document.getElementById('name').value;         
 }
 
+function addButton(){
+
+  if(isValid(money) && isValid(transaction)) {  
+    data.push({ 
+     transactions: transaction,           
+      detail:'income',  
+      price,money,
+
+    });  
+
+    display();
+  } 
+  alert(income)
+
+}
+
+
+
 function display(){
+
   balance.innerHTML= state.balance
   income.innerHTML= state.income
   expense.innerHTML= state.expense         
            
 }
-display();
 
 function isValid(value){
    if(!value) {
@@ -41,4 +51,14 @@ function isValid(value){
    return true;       
 }
 
-
+for (let i = 0; i < table.length; i++) {
+  tableRows.innerHTML += `
+  <tr>
+            <td>${table[i].id}</td>
+            <td>${table[i].transaction}</td>
+            <td>${table[i].type}</td>
+            <td>R${table[i].amount}</td>
+            <td><button class="remove" onClick="removeTransaction(${i})">Remove</button></td>
+          </tr>
+  `;
+}
